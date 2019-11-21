7ed689b280638bc00064b2c1f4b876e43b60925e
黑牛 <chenlm.nj@qq.com>
2019-11-21 17:24:40 +0800

    优化

--- algorithms/golang/64/minPathSum.go
+++ algorithms/golang/64/minPathSum.go
@@ -1,24 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func minPathSum(grid [][]int) int {
 
 	for i := len(grid) - 1; i >= 0; i-- {
 		for j := len(grid[i]) - 1; j >= 0; j-- {
-			if i+1 < len(grid) && j+1 < len(grid[i]) {
-				down := grid[i+1][j]
-				right := grid[i][j+1]
-				if down < right {
-					grid[i][j] = grid[i][j] + down
-				} else {
-					grid[i][j] = grid[i][j] + right
-				}
-			} else if i+1 < len(grid) {
-				down := grid[i+1][j]
+			down := down(&grid, i, j)
+			right := right(&grid, i, j)
+
+			if down != -1 && right != -1 {
+				grid[i][j] = grid[i][j] + min(down, right)
+			} else if down != -1 {
 				grid[i][j] = grid[i][j] + down
-			} else if j+1 < len(grid[i]) {
-				right := grid[i][j+1]
+			} else if right != -1 {
+
 				grid[i][j] = grid[i][j] + right
 			}
 
@@ -27,6 +25,27 @@ func minPathSum(grid [][]int) int {
 
 	return grid[0][0]
 
+}
+
+func down(grid *[][]int, i, j int) int {
+	if i+1 < len(*grid) {
+		return (*grid)[i+1][j]
+	}
+	return -1
+}
+
+func right(grid *[][]int, i, j int) int {
+	if j+1 < len((*grid)[i]) {
+		return (*grid)[i][j+1]
+	}
+	return -1
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func main() {
